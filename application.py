1c13f17e740e9fd011aa8441def078db2c37e7ef
Li Dai <lidaivet@gmail.com>
2018-10-10 22:28:21 -0400

    Minimal Viable Product Complete

--- application.py
+++ application.py
@@ -1,6 +1,4 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request, send_file
 from wsgiref.simple_server import make_server
 
 import logging
@@ -10,9 +8,13 @@ import resources
 
 application = Flask(__name__)
 
+@application.route('/resume/')
+def download_resume():
+	return send_file('./static/downloads/li_resume_10_08.pdf', as_attachment=True, attachment_filename='li_resume')
+
 @application.route('/', methods=['GET', 'POST'])
 def init():
     return render_template('index.html', text=resources.text)
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run()
--- resources.py
+++ resources.py
@@ -48,32 +48,149 @@ text={
 	},
 	'metrics':{
 		'metric_1':{
-			'name': '',
-			'value': ''
+			'name': 'Cities',
+			'value': '30'
 		},
 		'metric_2':{
-			'name': '',
-			'value': ''
+			'name': 'Countries',
+			'value': '6'
 		},
 		'metric_3':{
-			'name': '',
-			'value': ''
+			'name': 'Languages',
+			'value': '4'
 		},
 		'metric_4':{
-			'name': '',
-			'value': ''
-		},
+			'name': 'Careers',
+			'value': '2'
+		}
+
 	},
 	'skills':{
+		'skill_1':{
+			'name': 'Java',
+			'value': '97'
+		},
+		'skill_2':{
+			'name': 'C++',
+			'value': '94'
+		},
+		'skill_3':{
+			'name': 'Spring',
+			'value': '90'
+		},
+		'skill_4':{
+			'name': 'SQL',
+			'value': '90'
+		},
+		'skill_5':{
+			'name': 'AWS',
+			'value': '88'
+		},
+		'skill_6':{
+			'name': 'Unix',
+			'value': '89'
+		}
 
 	},
 	'education':{
+		'degree_1':{
+			'degree_name': 	'Master of Arts in Computer Science',
+			'story':		'When transitioned to my new career as a Queens College student, \
+							I participated in various club activities and volunteer work. \
+							Some interesting ones include Treasurer of Veteran\'s Club, \
+							Proctor for ACM Algorithm Competition, and Teaching Assistant for C++\
+							',
+			'courses': 	[	
+								'Advanced OOP in Java & C++', 
+						   		'Theory of Quantum Computing',
+							   	'Time & Space Complexity',
+							   	'Image Processing & Computer Graphics',
+							   	'Database Systems',
+								'Operating Systems & Distributed Systems',
+								'Machine Learning in Finance ',
+								'Algorithms & Data Structures'
+			]
+		},
+
+		'degree_2':{
+			'degree_name': 'Bachelor of Arts in Applied Mathematics & Psychology',
+			'story':		'Believed in Mathematics is human\'s way of understanding the universe, \
+							and Psychology is our way to understand human, I relentlessly pursued a double degree \
+							in these two in hope they will help me both the world and people better\
+							',
+			'courses':	[ 	
+								'Advanced Calculus',
+								'Differential Equation',
+								'Mathematics Problem Solving',	
+								'Probability & Statistics',
+								'Linear Algebra',
+								'Advanced Experimental Psychology',
+								'Psychometrics',
+								'Industrial & Organizational Psychology'
+			]
+		},
 
 	},
-	'experience':{
+	'direction': ['Left', 'Top', 'Right', 'Bottom'],
 
-	},
-	'work':{
+	'works':[
+		{
+			'title_company': 'Software Engineer at Fidelity Investments',
+			'time_range': 'Jan 2018 - Present',
+			'content': [
+				'Responsible for Adobe Analytics migration to AWS',
+				'Refactored E-mail templates to utlize functions of Apache Velocity template engine'
+				'Re-designing and developing existing Spring Batch process to prevent process failures',
+				'Acted as release captain to coordinate and perform scheduled software releases',
+				'Maintaining and monitoring existing linux servers and file systems',
+				'Designed and implemented algorithm for group level Hackathon',
+				'Decommissioning and cleaning up unused codes',
+				'Manipulating meta data on Oracle database to test existing batch process',
+				'Obtained AWS certification 2 months after knowing our team\'s cloud movement'
+			]
+		},
+		{
+			'title_company': 'Software Engineer Intern at Lift Rocket',
+			'time_range': 'Feb 2017 - Apr 2017',
+			'content': [
+				'Designed and developed transactional and analytical data structures',
+				'Modified existing software to correct Facebook login issue and improve performance',
+				'Worked closely with other team members to plan, design and develop robust solutions',
+				'Advocated for a hackathon to boost work progress by roughly 2 weeks',
+				'Wrote sql queries to support website function'
+			]
+		},
+		{
+			'title_company': 'Software Engineer Intern at Hooch Inc.',
+			'time_range': 'Jan 2016 - May 2016',
+			'content': [
+				'Self taught Ruby and Javascript to develop mobile applications in a fast pace startup environment',
+				'Integrated Company App with Amazon Echo by making a Alexa Skill using AWS, Javascript, and Python',
+				'Created a Hubot to improve communication between sales and technical team',
+				'Maintained backend database and servers using Ruby on Rails and AWS',
+				'Developed an automated email generating program using Ruby with gem SMTP',
+				'Fixed image error on Website within 2 weeks from not knowing WordPress to a problem solver'
+			]
+		},
+		{
+			'title_company': 'Aviation Electronics Specialist',
+			'time_range': 'Oct 2010 - Oct 2014',
+			'content': [
+				'Performed general maintenance on A10 aircrafts to ensure aircraft was mission capable',
+				'Organized government properties daily to keep track of aircraft maintenance record',
+				'Step promoted 6 month ahead to E-4. Top scorer for E-5 promotion',
+				'Supervised and trained DM Air Force Base Honor Guard to perform funerals, \
+				 change of commands, and color guard duties as a lead trainer',
+				'Maintained extreme professionalism and led by example',
+				'Managed and led approximated 150 airmen trainees as an airman leader at Sheppard Air Force base. \
+				 Squadron duties include: drills, duty scheduling, and safety training',
+				'Airman of the Quarter 2012 Spring, Distinguished Technical Training Graduate 2011, BMT Honor Graduate 2010',
+				'Obtained secret clearance'
+			]
+		},
+
+	],
+	'projects':{
 
 	},
 	'blog':{
@@ -82,4 +199,5 @@ text={
 	'contact':{
 
 	}
+
 }
