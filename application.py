16ac0174e6a19ada5aff9730e407ef444d96cdbb
lidai <lidaivet@gmail.com>
2020-03-13 00:40:51 -0400

    updated my site with a new pm version

--- application.py
+++ application.py
@@ -4,17 +4,27 @@ from wsgiref.simple_server import make_server
 import logging
 import logging.handlers
 import resources
-
+import resources_pm
 
 application = Flask(__name__)
 
+
 @application.route('/resume/')
 def download_resume():
-	return send_file('./static/downloads/li_resume.pdf', as_attachment=True, attachment_filename='li_resume.pdf')
+    return send_file('static/old/downloads/li_resume.pdf', as_attachment=True, attachment_filename='li_resume.pdf')
 
-@application.route('/', methods=['GET', 'POST'])
+
+@application.route('/old', methods=['GET'])
+def get_old():
+    return render_template('index_old.html', text=resources.text)
+
+
+@application.route('/', methods=['GET'])
 def init():
-    return render_template('index.html', text=resources.text)
+    return render_template('index.html', text=resources_pm.text)
+
 
 if __name__ == "__main__":
+    application.add_url_rule('/static_pm/<path:filename>', endpoint='static_pm',
+                             view_func=application.send_static_file)
     application.run()
---
+++ resources_pm.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+text = {
+    'types': [
+        'Wireframe',
+        'Prototype',
+        'Prioritization',
+        'Metrics',
+        'Product'
+    ],
+    'projects': [
+        {
+            'name': 'Baymax Wire Frame',
+            'type': 'Wireframe',
+            'image': 'baymax_wire.png'
+        },
+        {
+            'name': 'No Code Success Metric',
+            'type': 'Metrics',
+            'image': 'aarrr_metric.png'
+        },
+        {
+            'name': 'Jira Board Management',
+            'type': 'Prioritization',
+            'image': 'jira_sample.png'
+        },
+        {
+            'name': 'Prioritize by using MOSCOW',
+            'type': 'Prioritization',
+            'image': 'no_code_moscow.png'
+        },
+        {
+            'name': 'No Code Concept Mock Up',
+            'type': 'Prototype',
+            'image': 'no_mock_1.png'
+        },
+        {
+            'name': 'No Code Concept Mock Up',
+            'type': 'Prototype',
+            'image': 'no_mock_2.png'
+        }
+    ],
+}
